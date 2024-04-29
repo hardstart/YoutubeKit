21ad32c1c2a4a86d005c5fb23f4505ff6e330b5a
Hardik Rajput <46828696+hardstart@users.noreply.github.com>
2024-04-29 15:08:14 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "YoutubeKit",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .visionOS(.1)
     ],
     products: [
         .library(
